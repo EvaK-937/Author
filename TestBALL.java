9ff0dbedc51de864f29d396c6cab5465d1e1eccf
EvaK-937 <70647636+EvaK-937@users.noreply.github.com>
2020-09-13 14:51:31 +0300

    BALL
    PR2; lab.5

---
+++ TestBALL.java
@@ -0,0 +1,10 @@
+package ru.mirea.kicha0819.lab5;
+
+public class TestBALL {
+    public static void main (String[] args) {
+        BALL b1 = new BALL(60, 19);
+        System.out.println(b1);
+        b1.move(29, 32);
+        System.out.println(b1);
+    }
+}
